272bf355896f3396f52a9a01006665b5de31dccb
LucasCbz <bzprogrammation77@gmail.com>
2025-03-17 21:30:46 -0300

    ImplementandoListaAmigos

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
 function adicionandoAmigos() {
-  let amigoAtual = document.querySelector("input").value;
-  if (amigoAtual == "") {
+  let amigoEntrada = document.getElementById("amigo");
+  let amigoAtual = amigoEntrada.value;
+  if (amigoAtual === "") {
     alert("Por favor, insira um nome!");
   } else {
     amigos.push(amigoAtual);
-    amigoAtual = "";
+    amigoAtual.innerHTML = "";
+    criandoListaAmigos();
+  }
+}
+function criandoListaAmigos() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = amigos[i];
+    lista.appendChild(li);
   }
 }
 console.log(amigos);
